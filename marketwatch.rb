2431e1472322349b8bfa356f993582df5eca7876
Euank <euank@clemson.edu>
2014-01-04 15:22:22 -0500

    Fix login; do auth url callback

--- marketwatch.rb
+++ marketwatch.rb
@@ -18,22 +18,22 @@ class MarketWatch
     }.to_json
     resp = @c.post('https://id.marketwatch.com/auth/submitlogin.json', body, {'Content-Type' => "application/json; charset=UTF-8"})
     begin
-      if JSON.parse(resp.body)["result"] == "success"
-        @c.save_cookie_store
-        true
-      end
+      jsresp = JSON.parse(resp.body)
     rescue
-      false
+      return false
     end
+    unless jsresp["result"] == "success" && jsresp["url"]
+      return false
+    end
+
+    resp = @c.get(jsresp["url"])
+    resp.status == 200
   end
 
   def get_rankings(gamename)
-    # todo make this work. it doesn't
     url = "http://www.marketwatch.com/game/#{gamename}/ranking"
-    #@c.debug_dev = STDOUT
     html = @c.get_content(url)
     page = Nokogiri::HTML(html)
-    puts page
     ranking_section = page.css('.rankings').first
     puts ranking_section
     ranks = ranking_section.css('.tbody > tr > td.rank')
